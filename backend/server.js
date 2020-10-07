c0134d529f1ae8e7607a1a756a2ece8711901cec
João Gil <joaogmonteiro06@gmail.com>
2020-10-07 16:03:58 +0100

    Update socket

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const io = require("socket.io")(5000);
+const io = require("socket.io")("https://webchatnode.joaogil.dev:5000");
 
 io.on("connection", (socket) => {
   const id = socket.handshake.query.id;
